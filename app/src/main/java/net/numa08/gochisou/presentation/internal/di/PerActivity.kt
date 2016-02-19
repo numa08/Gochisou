4b046277d575869122eec66be4c0f7ccc1336e2b
numa08 <n511287@gmail.com>
2016-02-19 16:11:41 +0900

    fix project

--- app/src/main/java/net/numa08/gochisou/presentation/internal/di/PerActivity.kt
+++ app/src/main/java/net/numa08/gochisou/presentation/internal/di/PerActivity.kt
@@ -1,10 +1,8 @@
 package net.numa08.gochisou.presentation.internal.di
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
 import javax.inject.Scope
 
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class PerActivity
