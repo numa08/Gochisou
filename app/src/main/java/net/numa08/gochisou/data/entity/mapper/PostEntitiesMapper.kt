0a3b7dc988f2434e06abce63b46bb280018920e0
numa08 <n511287@gmail.com>
2016-02-19 14:11:09 +0900

    fix date format

--- app/src/main/java/net/numa08/gochisou/data/entity/mapper/PostEntitiesMapper.kt
+++ app/src/main/java/net/numa08/gochisou/data/entity/mapper/PostEntitiesMapper.kt
@@ -1,19 +1,26 @@
 package net.numa08.gochisou.data.entity.mapper
 
-import com.google.gson.*
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.GsonBuilder
+import com.google.gson.TypeAdapter
 import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
 import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.RealmString
+import org.apache.commons.lang3.time.DateFormatUtils
+import org.apache.commons.lang3.time.DateUtils
 import java.io.IOException
+import java.util.*
 import javax.inject.Singleton
 
 @Singleton
 class PostEntitiesMapper {
 
     internal val realmStringToken = object : TypeToken<RealmList<RealmString>>() {}.type
+    internal val dateToken = object : TypeToken<Date>() {}.type
 
     val gson = GsonBuilder().setExclusionStrategies(object : ExclusionStrategy {
         override fun shouldSkipField(f: FieldAttributes): Boolean {
@@ -24,19 +31,24 @@ class PostEntitiesMapper {
             return false
         }
     }).registerTypeAdapter(realmStringToken, object : TypeAdapter<RealmList<RealmString>>() {
-        @Throws(IOException::class)
-        override fun write(out: JsonWriter, value: RealmList<RealmString>) {
-        }
+        override fun write(p0: JsonWriter?, p1: RealmList<RealmString>?) {}
 
         @Throws(IOException::class)
-        override fun read(`in`: JsonReader): RealmList<RealmString> {
+        override fun read(`in`: JsonReader?): RealmList<RealmString>? = `in`?.let { i ->
             val list = RealmList<RealmString>()
-            `in`.beginArray()
-            while (`in`.hasNext()) {
-                list.add(RealmString(`in`.nextString()))
+            i.beginArray()
+            while(i.hasNext()) {
+                list.add(RealmString(i.nextString()))
             }
-            `in`.endArray()
-            return list
+            i.endArray()
+            list
         }
-    }).setDateFormat("yyyy-MM-dd'T'HH:mm:ssX").setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+    }).registerTypeAdapter(dateToken, object : TypeAdapter<Date>() {
+        override fun write(p0: JsonWriter?, p1: Date?) {}
+
+        override fun read(`in`: JsonReader?): Date? = `in`?.let { i ->
+            val pattern = DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT.pattern
+            DateUtils.parseDate(i.nextString(), pattern)
+        }
+    }).create()
 }
