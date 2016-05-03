914e60ecdfc49b311c937b40ea67428ba5604649
numa08 <n511287@gmail.com>
2016-05-03 18:26:33 +0900

    クラス定義の修正

--- app/src/main/java/net/numa08/gochisou/data/model/LoginProfile.kt
+++ app/src/main/java/net/numa08/gochisou/data/model/LoginProfile.kt
@@ -1,9 +1,15 @@
 package net.numa08.gochisou.data.model
 
 import org.parceler.Parcel
+import org.parceler.ParcelConstructor
+import org.parceler.ParcelProperty
 
 @Parcel(Parcel.Serialization.BEAN)
-data class LoginProfile(var teamURL: String = "", var token: String = "") {
+data class LoginProfile
+@ParcelConstructor
+constructor(@ParcelProperty("teamURL") val teamURL: String,
+            @ParcelProperty("token") val token: String) {
+
     val tokenForHeader : String
         get() = "Bearer $token"
 }
