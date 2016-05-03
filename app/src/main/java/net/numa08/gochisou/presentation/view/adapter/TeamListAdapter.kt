85c0d5e1177629f7280a9a18e290c290ded77308
numa08 <n511287@gmail.com>
2016-05-03 14:27:42 +0900

    realm 0.89.1

--- app/src/main/java/net/numa08/gochisou/presentation/view/adapter/TeamListAdapter.kt
+++ app/src/main/java/net/numa08/gochisou/presentation/view/adapter/TeamListAdapter.kt
@@ -5,17 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-
-import net.numa08.gochisou.data.model.Team
-
 import io.realm.RealmBaseAdapter
 import io.realm.RealmResults
+import net.numa08.gochisou.data.model.Team
 
 class TeamListAdapter(context: Context, realmResults: RealmResults<Team>?) : RealmBaseAdapter<Team>(context, realmResults, true) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val textView = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false) as TextView
-        textView.text = realmResults[position].name
+        val textView = (convertView as? TextView) ?: LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false) as TextView
+        textView.text = getItem(position).name
         return textView
     }
 }
