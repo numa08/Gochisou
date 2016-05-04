55b47523e33791e993ebdddc9e54f74a78c0120e
numa08 <n511287@gmail.com>
2016-05-04 12:26:13 +0900

    リポジトリーのリファクタリング

--- app/src/main/java/net/numa08/gochisou/data/repositories/LoginProfileRepositoryImpl.kt
+++ app/src/main/java/net/numa08/gochisou/data/repositories/LoginProfileRepositoryImpl.kt
@@ -1,45 +1,34 @@
 package net.numa08.gochisou.data.repositories
 
 import android.content.SharedPreferences
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import net.numa08.gochisou.data.model.LoginProfile
 import java.util.*
 
-class LoginProfileRepositoryImpl  constructor(val sharedPreferences: SharedPreferences) : LoginProfileRepository(
-   GsonBuilder().create().fromJson(sharedPreferences.getString(PREFERENCE_KEY, "[]"), object : TypeToken<ArrayList<LoginProfile>>(){}.type)
-) {
+class LoginProfileRepositoryImpl(override val sharedPreferences: SharedPreferences, val gson: Gson = GsonBuilder().create())
+: LoginProfileRepository(gson.fromJson(sharedPreferences.getString(PREFERENCE_KEY, "[]"), object : TypeToken<ArrayList<LoginProfile>>() {}.type)),
+        SharedPreferencesRepository {
 
     companion object {
         val PREFERENCE_KEY = "${LoginProfileRepositoryImpl::class.simpleName}.PREFERENCE_KEY"
     }
 
-    val gson = GsonBuilder().create()
+    override val preferenceKey: String
+            = PREFERENCE_KEY
 
-    override fun add(element: LoginProfile): Boolean = super.add(element).let {
-        write()
-        it
-    }
+    override fun add(element: LoginProfile): Boolean = super.add(element).apply { write() }
 
-    override fun addAll(elements: Collection<LoginProfile>): Boolean = super.addAll(elements).let {
-        write()
-        it
-    }
+    override fun addAll(elements: Collection<LoginProfile>): Boolean = super.addAll(elements).apply { write() }
 
-    override fun addAll(index: Int, elements: Collection<LoginProfile>): Boolean = super.addAll(index, elements).let {
-        write()
-        it
-    }
+    override fun addAll(index: Int, elements: Collection<LoginProfile>): Boolean = super.addAll(index, elements).apply { write() }
 
     override fun clear() {
         super.clear()
         write()
     }
 
-    private fun write() {
-        sharedPreferences.edit()
-                .putString(PREFERENCE_KEY, gson.toJson(this))
-                .apply()
-    }
-
+    override fun toJson(): String
+            = gson.toJson(this)
 }
--- app/src/main/java/net/numa08/gochisou/data/repositories/NavigationIdentifierRepositoryImpl.kt
+++ app/src/main/java/net/numa08/gochisou/data/repositories/NavigationIdentifierRepositoryImpl.kt
@@ -1,42 +1,33 @@
 package net.numa08.gochisou.data.repositories
 
 import android.content.SharedPreferences
+import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import net.numa08.gochisou.data.entity.mapper.NavigationIdentifierMapper
 import net.numa08.gochisou.data.model.NavigationIdentifier
 import java.util.*
 
-class NavigationIdentifierRepositoryImpl(val sharedPreferences: SharedPreferences) : NavigationIdentifierRepository(
-        NavigationIdentifierMapper().gson.fromJson(sharedPreferences.getString(PREFERENCE_KEY, "[]"), object : TypeToken<ArrayList<NavigationIdentifier>>() {}.type)
-), SharedPreferences.OnSharedPreferenceChangeListener {
+class NavigationIdentifierRepositoryImpl(override val sharedPreferences: SharedPreferences, val gson: Gson = NavigationIdentifierMapper().gson) : NavigationIdentifierRepository(
+        gson.fromJson(sharedPreferences.getString(PREFERENCE_KEY, "[]"), object : TypeToken<ArrayList<NavigationIdentifier>>() {}.type))
+        , SharedPreferences.OnSharedPreferenceChangeListener
+        , SharedPreferencesRepository {
     companion object {
         val PREFERENCE_KEY = "${NavigationIdentifierRepositoryImpl::class.simpleName}.PREFERENCE_KEY"
     }
-
-    val gson = NavigationIdentifierMapper().gson
 
     init {
         sharedPreferences.registerOnSharedPreferenceChangeListener(this)
     }
 
-    override fun add(element: NavigationIdentifier): Boolean = super.add(element).let {
-        write()
-        it
-    }
+    override fun add(element: NavigationIdentifier): Boolean = super.add(element).apply { write() }
 
-    override fun addAll(elements: Collection<NavigationIdentifier>): Boolean = super.addAll(elements).let {
-        write()
-        it
-    }
+    override fun addAll(elements: Collection<NavigationIdentifier>): Boolean = super.addAll(elements).apply { write() }
 
-    override fun addAll(index: Int, elements: Collection<NavigationIdentifier>): Boolean = super.addAll(index, elements).let {
-        write()
-        it
-    }
+    override fun addAll(index: Int, elements: Collection<NavigationIdentifier>): Boolean = super.addAll(index, elements).apply { write() }
 
-    override fun removeAt(index: Int): NavigationIdentifier = super.removeAt(index).let { write();it }
+    override fun removeAt(index: Int): NavigationIdentifier = super.removeAt(index).apply { write() }
 
-    override fun set(index: Int, element: NavigationIdentifier): NavigationIdentifier = super.set(index, element).let { write();it }
+    override fun set(index: Int, element: NavigationIdentifier): NavigationIdentifier = super.set(index, element).apply { write() }
 
     override fun move(from: Int, to: Int) {
         val i = super.removeAt(from)
@@ -60,15 +51,15 @@ class NavigationIdentifierRepositoryImpl(val sharedPreferences: SharedPreference
         })
     }
 
-    private fun write() {
-        sharedPreferences.edit()
-                .putString(PREFERENCE_KEY, gson.toJson(this))
-                .apply()
-    }
-
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         if(key == PREFERENCE_KEY) {
             sync()
         }
     }
+
+    override val preferenceKey: String
+            = PREFERENCE_KEY
+
+    override fun toJson(): String
+            = gson.toJson(this)
 }
